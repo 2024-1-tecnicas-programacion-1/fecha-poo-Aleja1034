530d07c8e757092f936aae78bda60fd721833afd
Aleja1034 <142344366+Aleja1034@users.noreply.github.com>
2024-04-22 23:42:50 -0500

    Update Principal.java

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,32 @@
-package com.mycompany.fecha.poo;
-
+package com.mycompany.Fecha.poo;
+import java.util.Scanner; 
+    
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    
+{
+    /**
+     * Complejidad temporal: O(N) - Tiempo lineal.
+     */
+    public static void main(String[] args) {
+        
+    
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Ingrese el día:");
+        int dia = sc.nextInt();
+        System.out.println("Ingrese el mes:");
+        int mes = sc.nextInt();
+        System.out.println("Ingrese el año:");
+        int ano = sc.nextInt();
+
+        Fecha fecha = new Fecha(dia, mes, ano);
+        if (Fecha.validarFecha()) {
+            System.out.println("La fecha es válida.");
+            System.out.println("Fecha corta: " + fecha.fechaCorta());
+            System.out.println("Fecha larga: " + fecha.fechaLarga());
+        } else {
+            System.out.println("Fecha no válida.");
+        }
+    }
+    
+}
 }
